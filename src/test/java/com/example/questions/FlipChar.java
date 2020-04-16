3cc2243c61e3c6506bce4ff5083d45a316202602
SmallNoc <850090614@qq.com>
2020-04-16 16:35:50 +0800

    每日一题

---
+++ src/test/java/com/example/questions/FlipChar.java
@@ -0,0 +1,28 @@
+package com.example.questions;
+
+import org.assertj.core.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FlipChar {
+    public static void main(String[] args) {
+        UpsideDown("hello     world   !");
+    }
+
+    /**
+     * 翻转字符串输出（）
+     * 20200410
+     */
+
+    protected static void UpsideDown(String str){
+        List  strList = Arrays.asList(str.trim().split(" +"));
+        Collections.reverse(strList);//翻转
+        System.out.println(String.join(" ", strList));//连接字符输出
+    }
+
+
+
+
+}
