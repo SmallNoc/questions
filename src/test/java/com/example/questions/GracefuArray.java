feab0f02a334dd14bab855d4f69508f7b014cf8e
SmallNoc <850090614@qq.com>
2020-04-23 00:13:54 +0800

    滑动窗口（数组问题）

--- src/test/java/com/example/questions/GracefuArray.java
+++ src/test/java/com/example/questions/GracefuArray.java
@@ -1,5 +1,19 @@
 package com.example.questions;
 
+/**
+ * 一个整数数组 nums 和一个整数 k。
+ *
+ * 如果某个 连续 子数组中恰好有 k 个奇数数字，我们就认为这个子数组是「优美子数组」。
+ *
+ * 请返回这个数组中「优美子数组」的数目。
+ *
+ *输入：nums = [1,1,2,1,1], k = 3
+ * 输出：2
+ * 解释：包含 3 个奇数的子数组是 [1,1,2,1] 和 [1,2,1,1] 。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-number-of-nice-subarrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class GracefuArray {
     public static void main(String[] args) {
         int nums[] = {1,3,2,5,2,2,2,6,3,2,1,2,2,2,1,2,8};
